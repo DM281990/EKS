8d4f9a93ba05fbfb965223d712656ba64bf0ee22
DIWAKAR_MISHRA_YODLEE_PC <diwakarmishra1990@gmail.com>
2024-09-02 01:54:33 +0530

    test

--- eks-demo/src/main/java/com/example/eksdemo/EksDemoApplication.java
+++ eks-demo/src/main/java/com/example/eksdemo/EksDemoApplication.java
@@ -1,13 +1,27 @@
 package com.example.eksdemo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class EksDemoApplication {
 
+	@Value("${app.username}")
+	private String username;
+	@Value("${app.password}")
+	private String password;
+
 	public static void main(String[] args) {
 		SpringApplication.run(EksDemoApplication.class, args);
+
+	}
+
+	@PostConstruct
+	public void init() {
+		System.out.print(username + "-------------" + password);
 	}
 
 }
